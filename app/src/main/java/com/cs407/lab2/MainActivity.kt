c273eacbd6390895ba7862b3a619bb48400f856b
hilbert0615 <hilbert0615@gmail.com>
2024-09-22 15:48:13 -0500

    All steps finished

--- app/src/main/java/com/cs407/lab2/MainActivity.kt
+++ app/src/main/java/com/cs407/lab2/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cs407.lab2
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val editText = findViewById<EditText>(R.id.myTextField)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener {
+            val userInput = editText.text.toString()
+//            Log.i("INFO", "Button Clicked!");
+            Toast.makeText(this, userInput, Toast.LENGTH_SHORT).show()
         }
     }
 }
